8408b672563f628d2a6746beaafecad4ae009a69
ivy <unjust@gmail.com>
2023-07-26 21:29:17 -0500

    corrections config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/demo-mdlinks/',
+  baseUrl: '/demo-mdlinks-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'davichobits', // Usually your GitHub org/user name.
-  projectName: 'demo-mdlinks', // Usually your repo name.
+  organizationName: 'Laboratoria', // Usually your GitHub org/user name.
+  projectName: 'demo-mdlinks-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
